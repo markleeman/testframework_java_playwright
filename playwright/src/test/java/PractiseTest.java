3000ec308c17efdbb51b2375a4d75cb7302f5ff3
Mark Leeman <16381328+markleeman@users.noreply.github.com>
2025-01-17 10:54:27 +0000

    Added simple login test

--- playwright/src/test/java/PractiseTest.java
+++ playwright/src/test/java/PractiseTest.java
@@ -2,9 +2,11 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.options.AriaRole;
 import org.testng.annotations.Test;
 
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -35,6 +37,34 @@ public class PractiseTest {
             // Expects page to have a heading with the name of Installation.
             assertThat(page.getByRole(AriaRole.HEADING,
                     new Page.GetByRoleOptions().setName("Installation"))).isVisible();
+        }
+    }
+
+    @Test
+    public void login() {
+
+        try (Playwright playwright = Playwright.create()) {
+
+            //PlaywrightAssertions.setDefaultAssertionTimeout(10_000);
+
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("https://the-deposit-service.sandbox.b7h.io/auth/login");
+
+            page.getByLabel("Email").fill("");
+            page.getByLabel("Password").fill("");
+
+            //page.getByLabel("Password").press("Enter");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+
+            page.waitForURL("https://the-deposit-service.sandbox.b7h.io");
+
+            page.screenshot(new Page.ScreenshotOptions()
+                    .setPath(Paths.get("screenshot.png"))
+                    .setFullPage(true));
+
+            assertThat(page).hasTitle(Pattern.compile("My Profile"));
+
         }
     }
 }
