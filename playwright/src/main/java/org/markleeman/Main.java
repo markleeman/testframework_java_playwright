48b7e54e22d791cb1f24e1f4184c06e76d654b4f
Mark Leeman <16381328+markleeman@users.noreply.github.com>
2025-01-17 10:19:31 +0000

    Moved example test to test folder

--- playwright/src/main/java/org/markleeman/Main.java
+++ playwright/src/main/java/org/markleeman/Main.java
@@ -1,7 +1,14 @@
 package org.markleeman;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
---
+++ playwright/src/test/java/PractiseTest.java
@@ -0,0 +1,40 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import org.testng.annotations.Test;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class PractiseTest {
+
+    @Test
+    public void simpletest() {
+
+        try (Playwright playwright = Playwright.create()) {
+
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+
+            // create a locator
+            Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+
+            // Expect an attribute "to be strictly equal" to the value.
+            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+
+            // Click the get started link.
+            getStarted.click();
+
+            // Expects page to have a heading with the name of Installation.
+            assertThat(page.getByRole(AriaRole.HEADING,
+                    new Page.GetByRoleOptions().setName("Installation"))).isVisible();
+        }
+    }
+}
